c197c755d58d70effe9970d616150b58d6d155be
Aayush  Karki <aayush@aiims.com.au>
2024-07-03 13:52:19 +1000

    files updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,17 +6,17 @@ import { i18n } from '../i18n-config';
 import { match as matchLocale } from '@formatjs/intl-localematcher';
 import Negotiator from 'negotiator';
 
-function getLocale(request: NextRequest): string | undefined {
-    // Negotiator expects plain object so we need to transform headers
-    const negotiatorHeaders: Record<string, string> = {};
-    request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+// function getLocale(request: NextRequest): string | undefined {
+//     // Negotiator expects plain object so we need to transform headers
+//     const negotiatorHeaders: Record<string, string> = {};
+//     request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
-    // Use negotiator and intl-localematcher to get best locale
-    let languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-    // @ts-ignore locales are readonly
-    const locales: string[] = i18n.locales;
-    return matchLocale(languages, locales, i18n.defaultLocale);
-}
+//     // Use negotiator and intl-localematcher to get best locale
+//     let languages = new Negotiator({ headers: negotiatorHeaders }).languages();
+//     // @ts-ignore locales are readonly
+//     const locales: string[] = i18n.locales;
+//     return matchLocale(languages, locales, i18n.defaultLocale);
+// }
 
 export function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
